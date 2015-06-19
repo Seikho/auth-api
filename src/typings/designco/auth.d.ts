aa32d76b89e05520a79e4f71169e3833465c1c55
Carl <carl@carlossus.com>
2015-06-19 19:03:37 +0800

    Adding auth api type definitions

---
+++ src/typings/designco/auth.d.ts
@@ -0,0 +1,13 @@
+export interface User {
+	displayName: string;
+	username: string;
+	email: string;
+	password: string;
+	enabled: number;
+	company: string;
+}
+
+export interface Login {
+	username: string;
+	password: string;
+}
