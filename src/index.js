2e53fb26838fb09944515420632f95aa0b90a161
Carl Winkler <carl.winkler86@gmail.com>
2015-06-21 19:03:58 +0800

    Server emits event on startup. Shuts down if database fails to init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 var cfg = require("ls-config");
 var dbInit = require("./store/init");
 var log = require("ls-logger");
+var store = require("ls-events");
+var webPort = cfg.config("webPort");
 cfg.config("baseDatabase", "auth.base.db");
 cfg.config("liveDatabase", "auth.db");
 dbInit()
-    .then(function (isCreated) {
-    log.info("Database created: " + isCreated);
-})
+    .then(successHandler)
     .catch(function (error) {
     log.error("Failed to created database: " + error);
+    process.exit(1);
 });
 log.info("Loading event handlers");
 require("./handlers/init");
+function successHandler(isCreated) {
+    log.info("Database created: " + isCreated);
+    var message = {
+        context: "services",
+        event: "start",
+        key: "auth",
+        data: { port: webPort }
+    };
+    store.pub(message)
+        .then(function () { return log.info("Server successfully started"); });
+}
+function failHandler(errorMessage) {
+    log.error("Failed to start server (Database error): " + errorMessage);
+    var message = {
+        context: "services",
+        event: "startError",
+        key: "auth",
+        data: { error: errorMessage }
+    };
+    store.pub(message)
+        .then(function () { return process.exit(1); });
+}
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,48 @@ import cfg = require("ls-config");
 import dbInit = require("./store/init");
 import log = require("ls-logger");
 import server = require("./server");
+import store = require("ls-events");
+
+var webPort = cfg.config("webPort");
 
 cfg.config("baseDatabase", "auth.base.db");
 cfg.config("liveDatabase", "auth.db");
 
 dbInit()
-    .then(isCreated => {
-        log.info("Database created: " + isCreated);
-    })
+    .then(successHandler)
     .catch(error => {
         log.error("Failed to created database: " + error);
+        
+        process.exit(1);
     });
 
 log.info("Loading event handlers");
 require("./handlers/init");
+
+function successHandler(isCreated: boolean) {
+    log.info("Database created: " + isCreated);
+    
+    var message = {
+        context: "services",
+        event: "start",
+        key: "auth",
+        data: { port: webPort }
+    };
+    
+    store.pub(message)
+        .then(() => log.info("Server successfully started"));
+}
+
+function failHandler(errorMessage: any) {
+    log.error("Failed to start server (Database error): " + errorMessage);
+    
+    var message = {
+        context: "services",
+        event: "startError",
+        key: "auth",
+        data: { error: errorMessage }
+    };
+    
+    store.pub(message)
+        .then(() => process.exit(1));
+}
