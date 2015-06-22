ef9f82a03dd5ef08aaa8bbb5afbdd74fd555cce7
Carl <carl@carlossus.com>
2015-06-22 19:34:12 +0800

    port in config is now set and published correctly

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import log = require("ls-logger");
 import server = require("./server");
 import store = require("ls-events");
 
-var webPort = cfg.config("webPort");
+var webPort = cfg.config("webPort", 10003);
 
 cfg.config("baseDatabase", "auth.base.db");
 cfg.config("liveDatabase", "auth.db");
@@ -18,28 +18,28 @@ require("./handlers/init");
 
 function successHandler(isCreated: boolean) {
     log.info("Database created: " + isCreated);
-    
+
     var message = {
         context: "services",
         event: "start",
         key: "auth",
         data: { port: webPort }
     };
-    
+
     store.pub(message)
         .then(() => log.info("Server successfully started"));
 }
 
 function failHandler(errorMessage: any) {
     log.error("Failed to start server (Database error): " + errorMessage);
-    
+
     var message = {
         context: "services",
         event: "startError",
         key: "auth",
         data: { error: errorMessage }
     };
-    
+
     store.pub(message)
         .then(() => process.exit(1));
 }
