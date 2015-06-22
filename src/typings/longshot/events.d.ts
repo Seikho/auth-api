4c4522ef7f07652dc4fa9e3c3b79f57fe2a0e7b9
Carl <carl@carlossus.com>
2015-06-22 19:58:22 +0800

    Updated events type definitions

--- src/typings/longshot/events.d.ts
+++ src/typings/longshot/events.d.ts
@@ -6,7 +6,7 @@ declare module "ls-events" {
 
 	export function client(): redis.RedisClient;
 	export function pub(event: Event): Promise<{}>;
-	export function psub(channel: string, callback: (channel: string, pattern: string, message: string) => void): Promise<{}>;
+	export function psub(channel: string, callback: (pattern: string, channel: string, message: EventData) => void): Promise<{}>;
 	export function sub(channel: string, callback: (channel: string, message: string) => void): Promise<{}>;
 	export function fetch(context?: string, event?: string, key?: string): Promise<any>;
 	export function setHost(hostname: string, port?: number);
@@ -16,5 +16,10 @@ declare module "ls-events" {
 		context: string;
 		data: any;
 		key: string|number;
+	}
+
+	export interface EventData {
+		published: number;
+		data: any;
 	}
 }
