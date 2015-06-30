d1dcaade0a712da9b2e2f8c53542ce851143555a
Carl <carl@carlossus.com>
2015-06-30 21:56:51 +0800

    createSession now emits an event on successful login

--- src/api/createSession.js
+++ src/api/createSession.js
@@ -1,10 +1,17 @@
 var db = require("../store/db");
+var store = require("ls-events");
 function createSession(username, token) {
     return db("sessions")
         .insert({ username: username, token: token })
         .then(function (id) {
         if (id.length > 0)
             return Promise.resolve(token);
+        store.pub({
+            event: "login",
+            context: "users",
+            key: username,
+            data: true
+        });
         return Promise.reject("Failed to insert new record: Unexpected error. Try again later.");
     })
         .catch(function (error) { return Promise.reject("Failed to insert new record: " + error); });
--- src/api/createSession.ts
+++ src/api/createSession.ts
@@ -1,4 +1,5 @@
 import db = require("../store/db");
+import store = require("ls-events");
 export = createSession;
 
 function createSession(username: string, token: string) {
@@ -6,6 +7,14 @@ function createSession(username: string, token: string) {
 		.insert({ username: username, token: token })
 		.then((id: number[]) => {
 			if (id.length > 0) return Promise.resolve(token);
+			
+			store.pub({
+				event: "login",
+				context: "users",
+				key: username,
+				data: true
+			});
+			
 			return Promise.reject("Failed to insert new record: Unexpected error. Try again later.");
 		})
 		.catch(error => Promise.reject("Failed to insert new record: " + error));
