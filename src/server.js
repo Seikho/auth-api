74aa6dffe5415bc4e8d1d11a2c3fb1f7de22e0c9
Carl Winkler <carl.winkler86@gmail.com>
2015-09-03 13:15:52 +0800

    Server no longer starts web server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 var express = require("express");
-var cfg = require("ls-config");
 var bodyParser = require("body-parser");
-var port = cfg.config("port") || cfg.config("p") || 10003;
 var server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
-server.listen(port);
 module.exports = server;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import express = require("express");
-import cfg = require("ls-config");
 var bodyParser = require("body-parser");
 export = server;
-
-var port = cfg.config("port") || cfg.config("p") || 10003;
 
 // Configure express
 var server = express();
 server.use(bodyParser.urlencoded({ extended: false }));
-server.listen(port);
